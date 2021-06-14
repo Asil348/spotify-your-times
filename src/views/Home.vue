2ed236650d9d42114dcb888e9979a176459daaf9
Asil <webdev348@gmail.com>
2021-06-15 01:22:52 +0300

    homepage styling

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,37 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <div class="container">
+        <button type="button" class="btn btn-lg btn-success"><i class="fab fa-spotify"></i>&nbsp;Can't do much without logging in with Spotify...</button>
+    </div>
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+<style lang="scss" scoped>
+  @import 'bootswatch/dist/zephyr/_variables.scss';
 
-export default defineComponent({
-  name: 'Home',
-  components: {
-    HelloWorld,
-  },
-});
+  .home {
+    height: 100vh;
+    background-color: darken($color: #1DB954, $amount: 5);
+  }
+
+  .home .container {
+    height: inherit;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
+
+<script lang="ts">
+  import {
+    defineComponent
+  } from 'vue';
+  import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+
+  export default defineComponent({
+    name: 'Home',
+    components: {
+      HelloWorld,
+    },
+  });
 </script>
