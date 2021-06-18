9e66b9b9fdce58be19963db9a05b8b3b06383ddd
Asil <webdev348@gmail.com>
2021-06-19 02:40:45 +0300

    integrate from ts to js

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,8 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import Callback from '../views/Callback.vue'
 
-const routes: Array<RouteRecordRaw> = [
+const routes = [
   {
     path: '/',
     name: 'Home',
@@ -14,6 +15,11 @@ const routes: Array<RouteRecordRaw> = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/callback',
+    name: 'Callback',
+    component: Callback
   }
 ]
 
