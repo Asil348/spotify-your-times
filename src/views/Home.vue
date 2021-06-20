8508e389917891d92c6ba4486deefb9dbad97dc5
Asil <webdev348@gmail.com>
2021-06-20 23:17:54 +0300

    fix auth bug

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,8 @@
 <template>
   <div class="home">
     <div class="container">
-      <a
-        href="https://accounts.spotify.com/authorize?client_id=2383a36b5aaa4b1d9ba46fd3383f0b56&response_type=code&redirect_uri=http://localhost:8080/callback&scope=playlist-read-collaborative"><button
-          type="button" class="btn btn-lg btn-success"><i class="fab fa-spotify"></i>&nbsp;Log in with
+      <a :href='request'><button type="button" class="btn btn-lg btn-success"><i class="fab fa-spotify"></i>&nbsp;Log in
+          with
           Spotify</button></a>
     </div>
   </div>
@@ -26,11 +25,31 @@
 </style>
 
 <script>
-
   export default {
     name: 'Home',
     components: {
       //HelloWorld,
+    },
+    data() {
+      return {
+        request: ``
+      }
+    },
+    mounted() {
+      this.request = `https://accounts.spotify.com/authorize?client_id=${process.env.VUE_APP_CLIENTID}&response_type=code&redirect_uri=${process.env.VUE_APP_REDIRECTURI}&scope=user-top-read` //%20user-read-email
+    },
+    methods: {
+      // Use this when you want to use 'state' on oauth
+      // generateState(length) {
+      //   let result = '';
+      //   let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+      //   let charactersLength = characters.length;
+      //   for (let i = 0; i < length; i++) {
+      //     result += characters.charAt(Math.floor(Math.random() *
+      //       charactersLength));
+      //   }
+      //   return result;
+      // }
     },
   };
 </script>
